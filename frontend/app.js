57023b3d4e632c148796f8845a1219f46342b2c6
Vanessa Cavaco <90183816+VanessaCavaco@users.noreply.github.com>
2024-01-02 17:45:52 +0100

    append html views

--- frontend/app.js
+++ frontend/app.js
@@ -3,7 +3,7 @@ import eventsData from './static/assets/data/events.json' assert { type: "json" 
 // Your SPA logic here...
 console.log(eventsData);
 
-
+//*********** other option to fectch the json file.
 // async function fetchData() {
 //     try {
 //       const response = await fetch('./static/assets/data/events.json');
@@ -19,4 +19,45 @@ console.log(eventsData);
 //   }
   
 //   fetchData();
+
+
+//*********** function to load the html pages
+const order = [
+  './static/html/AboutMe.html',
+  './static/html/Education.html',
+];
+
+const router = async () => {
+  const container = document.querySelector('#app');
+
+  // Load HTML files sequentially based on the predefined order
+  for (const page of order) {
+      const view = await fetchHtml(page);
+      appendHtml(container, view);
+      await sleep(1000); // Optional delay between loading each page
+  }
+};
+
+const fetchHtml = async (page) => {
+  const response = await fetch(page);
+  if (!response.ok) {
+      throw new Error(`Failed to fetch HTML: ${response.status}`);
+  }
+  return response.text();
+};
+
+const appendHtml = (container, html) => {
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = html;
   
+  // Append the content of the wrapper to the container
+  while (wrapper.firstChild) {
+      container.appendChild(wrapper.firstChild);
+  }
+};
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+// Trigger the router
+router();
+
