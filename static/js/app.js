3ab340c04f8f55891afa3b01b272db73c2fda4f7
Vanessa Cavaco <90183816+VanessaCavaco@users.noreply.github.com>
2024-04-14 16:04:00 +0200

    Keywords and line divs changes

--- static/js/app.js
+++ static/js/app.js
@@ -40,31 +40,42 @@ async function loadEventsContainer(eventType) {
   document.getElementById('app').insertAdjacentHTML('beforeend', eventsContainer);
 }
 //********** load years divs ************\\
-function addYear(year) {
-  // Create a new div element
-  let newYearDiv = document.createElement('div');
-  // Assign an id to the new div
-  newYearDiv.id = `${year}`;
-  // Assign a class year to the new div
-  newYearDiv.className = "year";
-  // Add content to the new div (optional)
-  //newYearDiv.textContent = `${year}`;
-    // Create a new div element for Experience, Education, Projects and line
-    let rightDiv = document.createElement('div');
-    let leftDiv = document.createElement('div');
-    let lineDiv = document.createElement('div');
-    //Assign the class for each div
-    rightDiv.className = "year-right";
-    leftDiv.className = "year-left";
-    lineDiv.className = "year-line";
-    //append to newYearDiv
-    newYearDiv.appendChild(rightDiv);
-    newYearDiv.appendChild(leftDiv);
-    newYearDiv.appendChild(lineDiv);
+function addYears(yearArray) {
   // Get the container div where you want to add the new div
   let containerDiv = document.getElementById('yearsStructure');
-  // Append the new div to the container div
-  containerDiv.appendChild(newYearDiv);
+  if(yearArray.length>0){
+    for(let year in yearArray){
+      // Create a new div element
+      let newYearDiv = document.createElement('div');
+      // Assign an id to the new div
+      newYearDiv.id = `${yearArray[year]}`;
+      // Assign a class year to the new div
+      newYearDiv.className = "year";
+      // Create a new div element for right and left and adds className
+      let rightDiv = document.createElement('div');
+      rightDiv.className = "year-right";
+      let leftDiv = document.createElement('div');
+      leftDiv.className = "year-left";
+      // Create a new div element for Line and add className
+      let lineDiv
+      if(year == 0){
+        lineDiv = document.createElement('div');
+        lineDiv.className = "year-lineTop";
+      }else if(year == yearArray.length-1){
+        lineDiv = document.createElement('div');
+        lineDiv.className = "year-lineBottom"; 
+      }else{
+        lineDiv = document.createElement('div');
+        lineDiv.className = "year-line"; 
+      }
+      //append to newYearDiv
+      newYearDiv.appendChild(rightDiv);
+      newYearDiv.appendChild(leftDiv);
+      newYearDiv.appendChild(lineDiv);
+      // Append the new div to the container div
+      containerDiv.appendChild(newYearDiv);
+    }
+  }
 }
 
 //********** add events to html **********\\     
@@ -95,7 +106,7 @@ async function addEvents() {
     <p>${event.where}</p>
     <p>${event.description}</p>
     ${event.more ? `<p>${event.more}</p>` : ''}
-    ${keywordsString ? `<p>${keywordsString}</p>` : ''}
+    ${keywordsString ? `<p class="keywords">${keywordsString}</p>` : ''}
     </div>
      `
     // Append the generated HTML to the container
@@ -119,9 +130,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   // loads Life page
   await loadEventsContainer("life");
   //add years divs
-  for(let year in uniqueYears){
-    addYear(uniqueYears[year]);
-  }
+  addYears(uniqueYears);
   //Load Events
   await addEvents();
 });
